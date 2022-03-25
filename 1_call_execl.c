5b9e854cdb70e9014c6d4a4004b7e1deb1ffbe2c
madh-prog <72149592+madh-prog@users.noreply.github.com>
2022-03-25 14:19:46 +0530

    Update 1_call_execl.c

--- 1_call_execl.c
+++ 1_call_execl.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-	execl("/home/mahima/mahima/einfochips/Final_project/linux_internals_assignment/process_assignment/1_exce_fd", "./1_exce_fd" , 0);
+	execl("/home/madhuri/Final_project/linux_internals_assignment/process_assignment/1_exce_fd", "./1_exce_fd" , 0);
 
 	return 0;
 }
